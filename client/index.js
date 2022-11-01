d10800e0087b6a62d8eeace3c3e286164e60132b
EdSheranaway <edenshirin@gmail.com>
2022-11-01 11:18:36 -0500

    fixing store bug
    Co-authored-by: Abigail Gerig <94636591+4estgirl@users.noreply.github.com>
    Co-authored-by: Trine Medina <106996307+TrineMedina@users.noreply.github.com>
    Co-authored-by: Adrian Reczek <101940529+adziu1234@users.noreply.github.com>
    Co-authored-by: Stephanie Page <108027476+vividvoltage@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-// import { store } from './store';
+import { store } from './app/store';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
