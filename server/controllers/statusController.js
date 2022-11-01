8ce3a5bd273afcd8f27928b4f7746e2890c7899d
EdSheranaway <edenshirin@gmail.com>
2022-11-01 17:07:32 -0500

    changing linter
    Co-authored-by: Abigail Gerig <94636591+4estgirl@users.noreply.github.com>
    Co-authored-by: Trine Medina <106996307+TrineMedina@users.noreply.github.com>
    Co-authored-by: Adrian Reczek <101940529+adziu1234@users.noreply.github.com>
    Co-authored-by: Stephanie Page <108027476+vividvoltage@users.noreply.github.com>

--- server/controllers/statusController.js
+++ server/controllers/statusController.js
@@ -1,70 +1,77 @@
-//TODO Add logic to access postgreSQL db
+// TODO Add logic to access postgreSQL db
 const db = require('../models/sqlConnection');
 
 const statusController = {};
 
-//Get all status
+// Get all status
 statusController.getAll = async (req, res, next) => {
-  //return an object of status w/id - this needs to be merged with the jobs
-  //in middleware downstream
+  // return an object of status w/id - this needs to be merged with the jobs
+  // in middleware downstream
   const querStr = 'SELECT * FROM public.jobs';
   try {
     const queryRes = await db.query(querStr);
     res.locals.allJobs = queryRes.rows;
     return next();
-
   } catch (error) {
-    return next({ log: `Error caught in statusController.getAll ${error}`,
-    status: 404,
-    message: 'No Jobs found!',})
+    return next({
+      log: `Error caught in statusController.getAll ${error}`,
+      status: 404,
+      message: 'No Jobs found!',
+    });
   }
-}
-//Gets archived status
-statusController.getArchived = () => {
-  //return an object of status archieved w/id - this needs to be merged with jobs
-  //in middleware downstream
+};
+// Gets archived status
+statusController.getArchived = (req, res, next) => {
+  // return an object of status archieved w/id - this needs to be merged with jobs
+  // in middleware downstream
   const querStr = '';
 
   try {
-    
+    return next();
   } catch (error) {
-    return next({ log: `Error caught in statusController.getArchived ${error}`,
-    status: 404,
-    message: 'No Archived Jobs found!',})
+    return next({
+      log: `Error caught in statusController.getArchived ${error}`,
+      status: 404,
+      message: 'No Archived Jobs found!',
+    });
   }
-}
+};
 
-statusController.updateStatus = () => {
-  //updates the status of job based on id in req.
-  //returns the job object with the new status on res.locals.job
-  //I don't think there's any necessity in accessing mongoDB
+statusController.updateStatus = (req, res, next) => {
+  // updates the status of job based on id in req.
+  // returns the job object with the new status on res.locals.job
+  // I don't think there's any necessity in accessing mongoDB
   const querStr = '';
 
   try {
-    
+    return next();
   } catch (error) {
-    return next({ log: `Error caught in statusController.updateStatus ${error}`,
-    status: 400,
-    message: 'Bad Request, enter valid update status',})
+    return next({
+      log: `Error caught in statusController.updateStatus ${error}`,
+      status: 400,
+      message: 'Bad Request, enter valid update status',
+    });
   }
-}
+};
 
-//Deletes status
-statusController.deleteStatus = () => {
-  //Locate job to be deleted on res.locals.job
-  //Delete status from DB
-  //Return res.locals.job w/ the ID of the deleted status (I think)
+// Deletes status
+statusController.deleteStatus = (req, res, next) => {
+  // Locate job to be deleted on res.locals.job
+  // Delete status from DB
+  // Return res.locals.job w/ the ID of the deleted status (I think)
   const querStr = '';
 
   try {
-    
+    return next();
   } catch (error) {
-    return next({ log: `Error caught in statusController.deleteStatus ${error}`,
-    status: 404,
-    message: 'Job does not exist',})
+    return next({
+      log: `Error caught in statusController.deleteStatus ${error}`,
+      status: 404,
+      message: 'Job does not exist',
+    });
   }
-}
+};
 
-//I had an option to get just specific status, but don't know if that's really necessary
+// I had an option to get just specific status, but don't know if that's really necessary
 
 module.exports = statusController;
--- server/routers/statusRouter.js
+++ server/routers/statusRouter.js
@@ -4,20 +4,16 @@ const statusController = require('../controllers/statusController');
 
 const router = express.Router();
 
-router.get('/', statusController.getAll, 
-(req, res, next) => {
+router.get('/', statusController.getAll, (req, res, next) => {
   return res.status(200).json(res.locals.allJobs);
-})
+});
 
-router.get('/', statusController.getAll, 
-(req, res, next) => {
+router.get('/', statusController.getAll, (req, res, next) => {
   return res.status(200).json(res.locals.allJobs);
-})
+});
 
-router.post('/update', statusController.updateStatus,
-  (req, res) => {
-  return res.status(200).json(res.locals.job)
-  })
+router.post('/update', statusController.updateStatus, (req, res) => {
+  return res.status(200).json(res.locals.job);
+});
 
 module.exports = router;
-
