f9d0eca4fcffb63768bd7024c4adbd6e1168daf5
TrineMedina <trinefirestone@gmail.com>
2022-11-01 18:03:49 -0400

    Added tests for creating a job
    Co-authored-by: Abigail Gerig <94636591+4estgirl@users.noreply.github.com>
    Co-authored-by: Trine Medina <106996307+TrineMedina@users.noreply.github.com>
    Co-authored-by: Adrian Reczek <101940529+adziu1234@users.noreply.github.com>
    Co-authored-by: Stephanie Page <108027476+vividvoltage@users.noreply.github.com>
    Co-authored-by: Eden Shirin <62536166+EdSheranaway@users.noreply.github.com>

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,11 +1,78 @@
-const request = require('supertest')
-const expect = require('chai')
+const supertest = require('supertest')
+mongoose = require ('mongoose')
+// const { describe, expect } = require(@jest/globals);
 const app = require('../server/server')
 
-describe('/status/update endpoint functionality', () => {
-  it('replies with an object when a POST request is sent', ()=>{
-    request(app).post('/status/update')
-      .expect('Content-Type', /application\/json/)
+const server = 'http://localhost:3000'
+
+beforeAll(done => {
+  done();
+})
+describe('It creates a job', () => {
+  it('responds with a status 200 and creates adds a job to the database', () => {
+    const body = {
+      // title: 'test title',
+      company: 'test company',
+      location: 'Silicon Valley',
+      link: 'www.google.com/givemeajob',
+      };
+    return supertest(server)
+      .post('/job/create')
+      .send(body)
       .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.title).toEqual('test title')
+        expect(res.body.id).toBeDefined();
+      })
+
   })
+})
+
+describe('It rejects creating a job missing a title', () => {
+  it('responds with a status 404', () => {
+    const body = {
+      company: 'test company',
+      location: 'Silicon Valley',
+      link: 'www.google.com/givemeajob',
+    };
+    return supertest(server)
+      .post('/job/create')
+      .send(body)
+      .expect(400)
+  })
+})
+
+describe('It rejects creating a job missing a location', () => {
+  it('responds with a status 404', () => {
+    const body = {
+      title: 'test title',
+      company: 'test company',
+      link: 'www.google.com/givemeajob',
+    };
+    return supertest(server)
+      .post('/job/create')
+      .send(body)
+      .expect(400)
+  })
+})
+
+describe('It rejects creating a job missing a link', () => {
+  it('responds with a status 404', () => {
+    const body = {
+      title: 'test title',
+      company: 'test company',
+      location: 'location'
+    };
+    return supertest(server)
+      .post('/job/create')
+      .send(body)
+      .expect(400)
+  })
+})
+
+
+afterAll(done => {
+  mongoose.connection.close();
+  done();
 })
